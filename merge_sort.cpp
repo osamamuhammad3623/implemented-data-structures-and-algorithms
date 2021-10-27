88d2607e782d5a8ebca74860db6f1d5075c3a599
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2021-10-27 10:28:32 +0200

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -9,7 +9,8 @@ void merge_(vector<int> vector1, vector <int> vector2, vector<int> result) {
 	int pos1{}, pos2{};
 	int size1 = vector1.size();
 	int size2 = vector2.size();
-
+	
+	//merge 2 arrays in case they are same size.
 	while (pos1 < size1 && pos2 < size2) {
 		if (vector1[pos1] < vector2[pos2]) {
 			result.push_back(vector1[pos1]);
@@ -25,7 +26,7 @@ void merge_(vector<int> vector1, vector <int> vector2, vector<int> result) {
 			pos1++, pos2++;
 		}
 	}
-	//the "while loop" merges 2 arrays in case they are same size.
+	
 	//the following if statements in case all elements in one array are merged and 
 	//there are some elements in the second array that aren't merges yet, so they're directly pushed
 	//into the merged vector
