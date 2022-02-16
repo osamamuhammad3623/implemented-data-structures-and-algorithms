b491a1da75014e06328468a07fe4fe2c3c0a5fcb
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2022-02-16 13:31:48 +0200

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -3,28 +3,27 @@
 #include <stack>
 using namespace std;
 
-void dfs(vector<vector<int>> g, int start){
-    cout << start << " ";
-    start--; // for 0-based indexing
+void dfs(vector<vector<int>> *g, int start){
+
     stack<int> st;
-    vector<bool> visited(g.size()+1, 0); // mark all nodes as unvisited
+    vector<bool> visited((*g).size(), 0); // mark all nodes as unvisited
 
     visited[start] = 1; // mark starting node as visited
-	
-    // add neighours to the stack
-    for (auto neighbour: g[start]){
+    cout << start << " ";
+
+    // add neighbours to the stack
+    for (auto neighbour: (*g)[start]){
         st.push(neighbour);
     }
 
     while(!st.empty()){
         int current = st.top();
-        current--; // // for 0-based indexing
         if (visited[current]){
             st.pop();
         }else{
-            cout << current+1 << " ";
+            cout << current << " ";
             visited[current] = 1;
-            for (auto neighbour: g[current]){
+            for (auto neighbour: (*g)[current]){
                 st.push(neighbour);
             }
         }
@@ -32,6 +31,13 @@ void dfs(vector<vector<int>> g, int start){
 }
 
 int main(void) {
-	vector<vector<int>> g = { { 2, 3, 4 }, { 1, 3 }, { 1, 2 }, { 1, 5, 6 }, {4}, {6} };
-	dfs(g, 1);
+	vector<vector<int>> g = {
+							{ 1,2 , 3},
+							{ 0,2},
+							{ 0,1 },
+							{ 0 ,4,5},
+							{3},
+							{3}
+							};
+	dfs(&g, 0);
 }
